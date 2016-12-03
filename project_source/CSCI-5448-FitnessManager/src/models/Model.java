66ebce6ce125bc87f9a47d361cbfeb5df1f6057b
chaitanya_soma <chaitanya.soma@colorado.edu>
2016-12-02 17:04:04 -0700

    testing

---
+++ project_source/CSCI-5448-FitnessManager/src/models/Model.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Model {
+
+	private int x;
+	public Model()
+	{
+		x = 0;
+	}
+	public Model (int x)
+	{
+		this.x = x;
+	}
+	public void incrementX()
+	{
+		x++;
+	}
+	public int getX()
+	{
+		return x;
+	}
+}
