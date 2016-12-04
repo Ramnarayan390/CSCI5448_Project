4b0fa4289805f6a170adf99a1bc0f539b14cf6e5
jbp173 <jordanpeters@comcast.net>
2016-12-03 19:00:13 -0700

    Added Client class, including the constructor and getters and setters

---
+++ project_source/CSCI-5448-FitnessManager/src/models/Client.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="soma_krishnamurthy_Client" )
+public class Client extends User {
+
+	@Column(name="Height")
+	public double height;
+	@Column(name="Weight")
+	public int weight;
+	@Column(name="Age")
+	public int age;
+
+	public Client()
+	{
+		;
+	}
+	public Client(double height, int weight, int age, String username, String name, String gender, String email, String location, String securityQuestion, String securityAnswer, String password)
+	{
+		super(username, name, gender, email, location, securityQuestion, securityAnswer, password);
+		this.height = height;
+		this.weight = weight;
+		this.age = age;	
+	}
+	
+	public double getHeight()
+	{
+		return this.height;
+	}
+	
+	public int getWeight()
+	{
+		return this.weight;
+	}
+	
+	public int getAge()
+	{
+		return this.age;
+	}
+	
+	public void setHeight(double height)
+	{
+		this.height = height;
+	}
+	
+	public void setWeight(int weight)
+	{
+		this.weight = weight;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+}
